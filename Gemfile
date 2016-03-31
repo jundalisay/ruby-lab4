7585984b01fd663aa19df486f0bbba3332fbbae3
jundalisay <jundalisay@yahoo.com>
2016-03-31 20:56:14 +0700

    stuck

--- Gemfile
+++ Gemfile
@@ -38,13 +38,14 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'rspec-rails'
+  gem 'puma'
 end
 
 group :production do
   # Access an IRB console on exception pages or by using <%= console %> in views
   # gem 'web-console', '~> 2.0'
-  gem 'puma'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'puma'
   gem 'spring'
 end
 
--- app/assets/javascripts/socket_chat.js
+++ app/assets/javascripts/socket_chat.js
@@ -2,7 +2,31 @@
 // please type it yourself, do NOT copy & paste
 
 window.ws = new WebSocket("ws://" + window.document.location.host)
-ws.onmessage = function(message) {
-	console.log("received:", message)
-};
+// ws.onmessage = function(message) {
+// 	console.log("received:", message)
+// };
 
+ws.onmessage = function(message) {
+      var data = JSON.parse(message.data)
+      // use data.content and data.user in the html text below:
+      var messageHtml = "<h3>chat message</h3>"
+      $(".messages").append(messageHtml)
+      // add some javascript animation here usng jQuery if you want
+    }
+
+function setupForm() {
+    $("form#new_message").on("submit", function(event) {
+      event.preventDefault();
+      var content = $("#message_content").val();
+      var user    = "user";
+      // let's use JSON to convert an object into string
+      ws.send(JSON.stringify({ content: content, user: user }));
+      // clear the input text:
+      $("#message_content").val('')
+  });
+}
+
+  // use page:change to be compatible with turbolinks
+  $(document).on("page:change", function(){
+    setupForm();
+  })
